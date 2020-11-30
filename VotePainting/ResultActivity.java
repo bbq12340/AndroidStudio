4cddee7664e253b257d007fb3ea73a57cd34f504
bbq12340 <58031020+bbq12340@users.noreply.github.com>
2020-11-30 14:33:25 +0900

    Update ResultActivity.java

--- VotePainting/ResultActivity.java
+++ VotePainting/ResultActivity.java
@@ -2,6 +2,7 @@ public class ResultActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result);
         setTitle("투표 결과");
@@ -13,22 +14,17 @@ public class ResultActivity extends AppCompatActivity {
         TextView tv[] = new TextView[imageName.length];
         RatingBar rbar[] = new RatingBar[imageName.length];
 
-        Integer tvID[] = {
-                R.id.iv1, R.id.iv2, R.id.iv3,
-                R.id.iv4, R.id.iv5, R.id.iv6,
-                R.id.iv7, R.id.iv8, R.id.iv9
-        };
-        Integer rbarID[] = {
-                R.id.rbar1, R.id.rbar2, R.id.rbar3,
-                R.id.rbar4, R.id.rbar5, R.id.rbar6,
-                R.id.rbar7, R.id.rbar8, R.id.rbar9
-        };
+        Integer tvID[] = { R.id.tv1, R.id.tv2, R.id.tv3, R.id.tv4, R.id.tv5,
+                R.id.tv6, R.id.tv7, R.id.tv8, R.id.tv9 };
+        Integer rbarID[] = { R.id.rbar1, R.id.rbar2, R.id.rbar3, R.id.rbar4,
+                R.id.rbar5, R.id.rbar6, R.id.rbar7, R.id.rbar8, R.id.rbar9 };
 
-        for (int i=0; i<voteResult.length; i++) {
+        for (int i = 0; i < voteResult.length; i++) {
             tv[i] = (TextView) findViewById(tvID[i]);
             rbar[i] = (RatingBar) findViewById(rbarID[i]);
         }
-        for (int i=0; i<voteResult.length; i++) {
+
+        for (int i = 0; i < voteResult.length; i++) {
             tv[i].setText(imageName[i]);
             rbar[i].setRating((float) voteResult[i]);
         }
@@ -37,7 +33,9 @@ public class ResultActivity extends AppCompatActivity {
         btnReturn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 finish();
+
             }
         });
+
     }
 }
